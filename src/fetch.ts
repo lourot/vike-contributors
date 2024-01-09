498f32cdc1419c7e58c36a750b73385a1655f578
Aurelien Lourot <software@lourot.dev>
2024-01-09 16:12:02 +0100

    Fetch contributors

--- src/fetch.ts
+++ src/fetch.ts
@@ -4,13 +4,19 @@ main();
 
 async function main() {
   const repos = await getRepos("vikejs");
-  console.log(repos);
+  for (const repo of repos) {
+    const contributors = await getContributors(repo);
+    console.log(contributors.length);
+  }
 }
 
-async function getRepos(org: string) {
-  const res = await fetch(`https://api.github.com/orgs/${org}/repos`);
-  const json = (await res.json()) as any;
-  return json.filter(
+type GithubRepo = {
+  contributors_url: string;
+};
+
+async function getRepos(org: string): Promise<GithubRepo[]> {
+  const repos = await fetchArray(`https://api.github.com/orgs/${org}/repos`);
+  return repos.filter(
     (repo: any) =>
       !repo.private &&
       !repo.fork &&
@@ -18,4 +24,26 @@ async function getRepos(org: string) {
       !repo.disabled &&
       !repo.is_template
   );
+}
+
+async function getContributors(repo: GithubRepo) {
+  return await fetchArray(repo.contributors_url);
+}
+
+async function fetchArray(url: string): Promise<any[]> {
+  const result: any[] = [];
+  let page = 1;
+  while (true) {
+    const res = await fetch(url + "?per_page=100&page=" + page);
+    const json = (await res.json()) as any;
+    result.push(...json);
+    if (json.length < 100) {
+      break; // that was the last page
+    }
+    ++page;
+    if (page > 10) {
+      throw new Error("too many pages");
+    }
+  }
+  return result;
 }
